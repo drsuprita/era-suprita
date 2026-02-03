aa86829238dc266a1be39f77c8dcd0a20c924d4d
drsuprita <supritabelagali2004@gmail.com>
2026-02-03 15:48:24 +0530

    commit workspace changes

---
+++ bill.py
@@ -0,0 +1,8 @@
+total_bill = float(input("Enter the total bill amount: "))
+num_people = int(input("Enter the number of people: "))
+share_per_person = total_bill / num_people
+print(f"Total Bill: {total_bill}. Each person pays: {share_per_person}")
+
+print("Data type of total_bill:", type(total_bill))
+print("Data type of num_people:", type(num_people))
+print("Data type of share_per_person:", type(share_per_person))
